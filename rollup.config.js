722e04e12c3c4cda6eb07ae665b23bfaa2d91932
tan3li <lilja.taneli@gmail.com>
2025-06-27 09:42:49 +0300

    asdf

--- rollup.config.js
+++ rollup.config.js
@@ -1,41 +1,25 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-import svgr from '@svgr/rollup';
-import dts from 'rollup-plugin-dts';
-import postcss from 'rollup-plugin-postcss';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import copy from 'rollup-plugin-copy';
-import del from 'rollup-plugin-delete';
-
-export default [
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const pkg = require('./package.json');
+export default {
+  input: 'src/index.ts',
+  external: ['react', 'react-dom'],
+  output: [
     {
-        input: 'src/index.ts',
-        output: [
-            {
-                file: 'dist/index.js',
-                format: 'esm',
-                sourcemap: true
-            }
-        ],
-        plugins: [
-            peerDepsExternal(),
-            resolve(),
-            commonjs(),
-            typescript({tsconfig: './tsconfig.build.json'}),
-            svgr({icon: true}),
-            postcss({
-                extract: 'index.css'
-            }),
-            copy({
-                targets: [{src: 'public/locales/en/translation.json', dest: 'dist'}]
-            })
-        ]
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true
     },
     {
-        input: 'dist/dts/index.d.ts',
-        output: [{file: 'dist/index.d.ts', format: 'esm'}],
-        plugins: [dts(), del({hook: 'buildEnd', targets: 'dist/dts'})],
-        external: [/\.(css|less|scss)$/]
+      file: pkg.module,
+      format: 'esm',
+      sourcemap: true
     }
-];
+  ],
+  plugins: [
+    typescript({
+      tsconfig: './tsconfig.json'
+    })
+  ]
+};
